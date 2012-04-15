fc4d1db4741d0abeb6aae2d1a6cd0671485055c3
Michael Drogalis <mjd3089@rit.edu>
2012-04-14 22:23:44 -0400

    Generated project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TucsonElite::Application.config.secret_token = '663e2fba29a54b09bbd00501e43ae0f3764391cb554e12e0eab138ed5cf5702d81cf46286874af912766624e809f5a96e2754c4722de7fa336c412dcc2156209'
