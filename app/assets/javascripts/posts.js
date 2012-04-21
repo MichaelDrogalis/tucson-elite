fb083644eded6d02e4cbe20a5fb7b4d817c2e096
Michael Drogalis <mjd3089@rit.edu>
2012-04-20 23:21:38 -0400

    Stubbed function in for expanding heights.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
     
     $(postContentSelector).keyup(function() {
 	populatePreviewArea($(this));
+        syncInputAndPreviewHeights();
     });
 
     function populatePreviewArea(context) {
         var input = context.val();
         var previewHtml = markdown.toHTML(input);
         $("#post-preview").html(previewHtml);	
+    }
+
+    function syncInputAndPreviewHeights() {
+	
     }
 });
