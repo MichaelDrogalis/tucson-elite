835e34d7fece3699099d90ad9ea6dd4dd6fd6bc0
Michael Drogalis <mjd3089@rit.edu>
2012-04-20 23:15:51 -0400

    Populate preview area as soon as the page loads.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    $("#post-content-input").keyup(function() {
-        var input = $(this).val();
-        var previewHtml = markdown.toHTML(input);
-        $("#post-preview").html(previewHtml);
+    var postContentSelector = $("#post-content-input");
+    populatePreviewArea(postContentSelector);
+    
+    $(postContentSelector).keyup(function() {
+	populatePreviewArea($(this));
     });
+
+    function populatePreviewArea(context) {
+        var input = context.val();
+        var previewHtml = markdown.toHTML(input);
+        $("#post-preview").html(previewHtml);	
+    }
 });
