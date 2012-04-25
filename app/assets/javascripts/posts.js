4221beb268d1e90a494e076385bbd724e4ce221b
Michael Drogalis <mjd3089@rit.edu>
2012-04-25 16:13:00 -0400

    Back to top button for blog posts.

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -20,4 +20,16 @@ $(document).ready(function() {
         var postPreview = $("#post-preview");
         postContentSelector.height(postPreview.height());
     }
+
+    $(window).scroll(function() {
+	if($(this).scrollTop() != 0) {
+	    $('#toTop').fadeIn();
+	} else {
+	    $('#toTop').fadeOut();
+	}
+    });
+ 
+    $('#toTop').click(function() {
+	$('body,html').animate({scrollTop:0},800);
+    });
 });
