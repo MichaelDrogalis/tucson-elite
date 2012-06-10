8317ed2559c6417dc6f3a1450a2edbccad4d85c4
Michael Drogalis <mjd3089@rit.edu>
2012-06-10 12:32:55 -0400

    Using a simpler approach to show the tag links.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all.sort_by { |post| post.created_at }.reverse
+    @tags = Post.tag_counts.map do |pair| pair.name end
 
     respond_to do |format|
       format.html # index.html.erb
