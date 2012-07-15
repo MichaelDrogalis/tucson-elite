595e46007e6ed3ea4bc105b30f84fe93863c0e3b
Michael Drogalis <mjd3089@rit.edu>
2012-07-14 21:06:14 -0400

    Tip material.

---
+++ app/models/tip.rb
@@ -0,0 +1,4 @@
+class Tip < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ TucsonElite::Application.routes.draw do
   resources :posts
   resources :post_comments
   resources :videos
+  resources :tips
 
   get "/welcome/index"
   get "/about" => "about#index", :as => :about
   get "/founders" => "founders#index", :as => :founders
   get "/blog" => "posts#index", :as => :blog
+  get "/tips" => "tips#index", :as => :tips
 
   get "/post_tags/:tag" => "post_tags#index", :as => :post_tags
   get "/video_tags/:tag" => "video_tags#index", :as => :video_tags
@@ -18,6 +20,7 @@ TucsonElite::Application.routes.draw do
   get "/posts_admin" => "posts#listing", :as => :posts_listing
   get "/post_comments_admin" => "post_comments#listing", :as => :post_comments_listing
   get "/videos_admin" => "videos#listing", :as => :videos_listing
+  get "/tips_admin" => "tips#listing", :as => :tips_listing
   
   root :to => "welcome#index"  
 end
